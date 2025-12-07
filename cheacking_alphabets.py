a58febdf933dbaeec4f8ce7cb267708b502768f7
sia240914 <kushwahasia@gmail.com>
2025-12-07 16:04:55 +0530

    cheacking alphabets

---
+++ cheacking_alphabets.py
@@ -0,0 +1,5 @@
+character=str(input("enter your character"))
+if((character>='a'and character<='z') or (character>='A' and character<='Z')):
+    print("an alphabet")
+else:
+    print("isn't an alphabet")
