011485f3add4aba03617d6ee44a5fc4a04a70cb6
sia240914 <kushwahasia@gmail.com>
2025-12-07 15:27:37 +0530

    upper case lower case conversion

---
+++ case_conversion.py
@@ -0,0 +1,20 @@
+wishes="congratulations for getting gold medal in the state games"
+upper_wishes=wishes.upper()
+print(upper_wishes)
+
+lower_wishes=wishes.lower()
+print(lower_wishes)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
