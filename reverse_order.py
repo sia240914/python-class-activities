0a2dbd7f3e76023090bdd8466e56e6accc26fbf3
sia240914 <kushwahasia@gmail.com>
2025-12-13 11:01:09 +0530

    loops

---
+++ reverse_order.py
@@ -0,0 +1,4 @@
+num=int(input("enter your number"))
+
+for i in range (num,0,-1):
+    print(i)
---
+++ reverse_str.py
@@ -0,0 +1,6 @@
+string=input("enter your string : ")
+string2=('')
+for i in string:
+    string2=i+string2
+    
+print(string2)
