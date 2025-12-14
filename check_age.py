2bbfeed99aca0980c23497813c00bfe9b2e65171
sia240914 <kushwahasia@gmail.com>
2025-12-14 16:12:55 +0530

    age

---
+++ check_age.py
@@ -0,0 +1,6 @@
+age=int(input("enter your age"))
+
+if (age>=10 and age<=20):
+    print("you can enter in the class")
+else:
+    print("you cannot enter in the class")
--- num_swap.py
+++ num_swap.py
@@ -1,10 +1,8 @@
 num1=int(input("enter your number 1 "))
 num2=int(input("enter your number 2 "))
-num3=int(input("enter your number 3 "))
 temp=0
-print(num1, num2, num3)   
+print(num1, num2)   
 temp=num1
-num1=num3
-num3=num2
+num1=num2
 num2=temp
-print(num1, num2, num3)
+print(num1, num2)
