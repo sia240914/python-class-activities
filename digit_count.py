e8407d41ffa2455936396f8778db887acc67468f
sia240914 <kushwahasia@gmail.com>
2025-12-20 17:31:36 +0530

    while loops

---
+++ digit_count.py
@@ -0,0 +1,33 @@
+count=0
+num=int(input("enter your number"))                                               
+
+while (num>0):
+    quotiont=num//10
+    count=count+1
+    num=quotiont
+print("digits count:",count)
+
+# quotiont=num//10
+# count=count+1
+# print("Count:", count)
+# print(quotiont)
+# num=quotiont
+# quotiont=num//10
+# count=count+1
+# print("Count:", count)
+# print(quotiont)
+# num=quotiont
+# quotiont=num//10
+# count=count+1
+# print("Count:", count)
+# print(quotiont)
+# num=quotiont
+# quotiont=num//10
+# count=count+1
+# print("Count:", count)
+# print(quotiont)
+# num=quotiont
+# quotiont=num//10
+# count=count+1
+# print("Count:", count)
+# print(quotiont)
