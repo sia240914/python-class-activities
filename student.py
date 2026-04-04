c9ae614f8fa50b3b251815d5fd2e26314de7a9ef
sia240914 <kushwahasia@gmail.com>
2026-04-04 10:49:03 +0530

    classob

---
+++ student.py
@@ -0,0 +1,4 @@
+class student:
+    grade=10
+    print("this is a student from",grade)
+s1=student()
---
+++ vehicle.py
@@ -0,0 +1,7 @@
+class vehicle :
+    def __init__(self,milage,max_speed):
+        self.milage=milage
+        self.max_speed=max_speed
+v1=vehicle(500,20)
+print("the milage is ", v1.milage)
+print("the max speed is ", v1.max_speed)
