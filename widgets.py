c56a3fd8f1b8e590ddbdf185800fd33da8fc8334
sia240914 <kushwahasia@gmail.com>
2026-05-30 14:39:48 +0530

    wiget

--- widgets.py
+++ widgets.py
@@ -1,45 +1,41 @@
 from tkinter import *
-from datetime import date
+import math
 window= Tk()
 
 window.title("getting started with widgets")
-window.geometry('300x300')
+window.geometry('500x400')
 lb1=Label(text=" hey there",fg="#8D61B5",bg="white",height=5,width=10)
 
-name_lb1=Label(text="enter your full name",bg="#BBB0FF")
-name_entry=Entry()
+name_lb1=Label(text="enter digit 1 ",bg="#C7C2E4")
+name_entry1=Entry()
+name_lb2=Label(text="enter digit 2",bg="#2C0FE6")
+name_entry2=Entry()
+
+
+
+
+
+
 
 
 def display():
-    name=name_entry.get()
+    num1=float(name_entry1.get())
+    num2=float(name_entry2.get())
+    producter=(num1 * num2)
     global message
-    message="welcome to this application \n todays date is "
-    greet="hello "+name+"\n"
+    message="the product of the two numbers is :",producter
+    greet="hello "
+    
     text_box.insert(END,greet)
     text_box.insert(END,message)
-    text_box.insert(END,date.today())
 text_box=Text(height=10)
-button=Button(text="begin",fg="#5316C5",bg="white",command=display)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+button=Button(text="answer",fg="#5316C5",bg="white",command=display)
 
 lb1.pack()
 name_lb1.pack()
-name_entry.pack()
+name_entry1.pack()
+name_lb2.pack()
+name_entry2.pack()
 text_box.pack()
 button.pack()
 
